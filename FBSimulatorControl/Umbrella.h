942031606705357d6654af388f6226cc4c67edd5
Geoff <blotter@ldschurch.org>
2015-11-12 12:27:53 -0700

    Made the framework a modular framework that I can pull in to a demo app

---
+++ FBSimulatorControl/Umbrella.h
@@ -0,0 +1,95 @@
+//
+//  Umbrella.h
+//  FBSimulatorControl
+//
+//  Created by Geoffrey Blotter on 11/11/15.
+//
+//
+
+#ifndef Umbrella_h
+#define Umbrella_h
+
+#import "FBSimulator.h"
+#import "FBSimulator+Private.h"
+#import "FBSimulator+Queries.h"
+#import "FBSimulatorControl.h"
+#import "FBSimulatorControl+Private.h"
+#import "FBSimulatorInteraction.h"
+#import "FBSimulatorInteraction+Private.h"
+#import "FBSimulatorPool.h"
+#import "FBSimulatorPool+Private.h"
+#import "FBSimulatorPredicates.h"
+
+
+//#import "FBSimulatorControl+Private.h"
+#import "FBSimulatorLogs+Private.h"
+#import "FBSimulatorControlConfiguration.h"
+#import "FBTask+Private.h"
+#import "FBSimulatorSessionInteraction+Diagnostics.h"
+#import "FBSimulatorPool+Private.h"
+#import "FBSimulatorSessionStateGenerator.h"
+#import "FBSimulatorProcess.h"
+#import "FBSimulatorSessionState.h"
+#import "FBSimulatorSessionLifecycle.h"
+#import "FBTerminationHandle.h"
+#import "FBTask.h"
+#import "FBSimulatorSession+Convenience.h"
+#import "FBSimulatorSessionState+Queries.h"
+#import "FBSimulatorProcess+Private.h"
+#import "NSRunLoop+SimulatorControlAdditions.h"
+#import "FBSimulatorConfiguration+Convenience.h"
+#import "FBSimulatorInteraction+Private.h"
+#import "FBSimulatorPool.h"
+#import "FBProcessLaunchConfiguration.h"
+#import "FBSimulatorWindowTiler.h"
+#import "FBSimulatorSessionInteraction+Private.h"
+#import "FBSimulator+Queries.h"
+#import "FBSimulatorInteraction.h"
+#import "FBSimulatorSessionInteraction.h"
+#import "FBSimulatorSession.h"
+#import "FBSimulatorApplication.h"
+#import "FBTaskExecutor.h"
+#import "FBSimulatorLogger.h"
+#import "FBCoreSimulatorNotifier.h"
+#import "FBSimulatorConfiguration+CoreSimulator.h"
+#import "FBSimulatorSession+Private.h"
+#import "FBSimulatorConfiguration.h"
+#import "FBProcessLaunchConfiguration+Private.h"
+#import "FBTaskExecutor+Convenience.h"
+#import "FBSimulatorConfiguration+Private.h"
+#import "FBSimulator+Private.h"
+#import "FBSimulatorVideoRecorder.h"
+#import "FBSimulatorVideoUploader.h"
+#import "FBSimulatorControlStaticConfiguration.h"
+#import "FBTaskExecutor+Private.h"
+#import "FBSimulatorWindowTilingStrategy.h"
+#import "FBSimulatorPredicates.h"
+#import "FBDispatchSourceNotifier.h"
+#import "FBSimulatorControl.h"
+#import "FBConcurrentCollectionOperations.h"
+#import "FBSimulatorWindowHelpers.h"
+#import "FBSimulatorLogs.h"
+#import "FBWritableLog.h"
+#import "FBWritableLog+Private.h"
+#import "FBInteraction.h"
+#import "FBSimulatorError.h"
+#import "FBInteraction+Private.h"
+#import "FBProcessLaunchConfiguration+Helpers.h"
+#import "FBSimulator.h"
+#import "FBSimulatorSessionState+Private.h"
+
+
+
+#endif /* Umbrella_h */
+
+
+//#import <Management/FBSimulator.h>
+//#import <Management/FBSimulator+Private.h>
+//#import <Management/FBSimulator+Queries.h>
+//#import <Management/FBSimulatorControl.h>
+//#import <Management/FBSimulatorControl+Private.h>
+//#import <Management/FBSimulatorInteraction.h>
+//#import <Management/FBSimulatorInteraction+Private.h>
+//#import <Management/FBSimulatorPool.h>
+//#import <Management/FBSimulatorPool+Private.h>
+//#import <Management/FBSimulatorPredicates.h>
